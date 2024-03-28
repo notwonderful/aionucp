0c4b9e75648ea4520ef37ce02ca850f8c7d2a1cf
notwonderful <ratatatata322@gmail.com>
2024-03-28 12:28:26 +0300

    Chore: Add teleport coordinates to .env

--- config/teleport.php
+++ config/teleport.php
@@ -2,15 +2,15 @@
 
 return [
     'elyos' => [
-        'x' => env('TELEPORT_ELYOS_X', 123.45),
-        'y' => env('TELEPORT_ELYOS_Y', 567.89),
-        'z' => env('TELEPORT_ELYOS_Z', 101.12),
-        'map' => env('TELEPORT_ELYOS_MAP', 1),
+        'x' => env('TELEPORT_ELYOS_X'),
+        'y' => env('TELEPORT_ELYOS_Y'),
+        'z' => env('TELEPORT_ELYOS_Z'),
+        'map' => env('TELEPORT_ELYOS_MAP'),
     ],
     'asmodians' => [
-        'x' => env('TELEPORT_ASMODIANS_X', 456.78),
-        'y' => env('TELEPORT_ASMODIANS_Y', 901.23),
-        'z' => env('TELEPORT_ASMODIANS_Z', 456.78),
-        'map' => env('TELEPORT_ASMODIANS_MAP', 2),
+        'x' => env('TELEPORT_ASMODIANS_X'),
+        'y' => env('TELEPORT_ASMODIANS_Y'),
+        'z' => env('TELEPORT_ASMODIANS_Z'),
+        'map' => env('TELEPORT_ASMODIANS_MAP'),
     ],
 ];
