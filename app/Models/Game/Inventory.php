d677873affb103d37628c07efee36b4c160a45c3
notwonderful <ratatatata322@gmail.com>
2024-04-07 15:33:42 +0300

    Core: Add Inventory Model

---
+++ app/Models/Game/Inventory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Game;
+
+class Inventory extends BaseGameModel
+{
+    protected $table = 'inventory';
+
+    protected $primaryKey = 'item_unique_id';
+
+    protected $fillable = [
+        'item_unique_id',
+        'item_id',
+        'item_skin',
+        'item_count',
+        'item_owner',
+        'item_creator',
+        'item_location',
+        'enchant',
+        'authorize',
+    ];
+}
