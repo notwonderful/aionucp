08fbddb244c3b6a5dad087cbc6c702de8356cf66
notwonderful <ratatatata322@gmail.com>
2024-04-15 21:58:15 +0300

    Force HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,11 +6,22 @@ use App\Enums\UserRole;
 use App\Listeners\CreateReferralLink;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+
     /**
      * Bootstrap any application services.
      */
