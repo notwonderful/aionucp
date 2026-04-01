613293b461b0b1f2b05e4ee9ca3092497e86fd65
Nik <102656911+notwonderful@users.noreply.github.com>
2026-04-01 14:06:23 +0300

    feat: enable strict model mode in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,32 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use App\Enums\UserRole;
 use App\Listeners\CreateReferralLink;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
 final class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        Model::shouldBeStrict();
+
         Gate::define('admin', function (User $user) {
             return $user->role === UserRole::ADMIN;
         });
